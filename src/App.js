c13bd10628af969f3148d39c3fac9bdf81230335
Itamar <itamarmedeiros@gmail.com>
2024-03-18 16:17:18 -0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Navbar from './Navbar';
-import Home from './Home'
+import Home from './Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
